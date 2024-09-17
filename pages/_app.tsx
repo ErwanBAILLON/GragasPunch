c251a6e114846e7a5de032450488b54d1fe77fbb
nlouis56 <nataniel-.louis@epitech.eu>
2024-09-17 13:39:39 +0200

    changing the metadata to speak english now

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,8 +6,8 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Grasapunch</title>
-        <meta property="og:title" content="Grasapunch - Boissons énergisantes naturelles" />
-        <meta name="description" content="Découvrez notre gamme de boissons énergisantes naturelles à base d'Arasgrasas. Boostez votre énergie avec des ingrédients sains et naturels. Commandez en ligne dès maintenant !" />
+        <meta property="og:title" content="Grasapunch - Natural Energy Drinks" />
+        <meta name="description" content="Discover our range of natural energy drinks made with Arasgrasa. Boost your energy with healthy and natural ingredients. Order online now!" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta property="og:url" content="https://www.grasapunch.live" />
         <link rel="icon" href="/logo_grasapunch.png" />
